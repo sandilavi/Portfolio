bae463afaf336d7c91cfe3fbc00e3608bc4f13e6
sandilavi <sandilavihanga@gmail.com>
2023-09-26 19:49:12 +0530

    Tried to fix error 404 by adding netlify.toml file

--- src/components/contact_me/Contacts.jsx
+++ src/components/contact_me/Contacts.jsx
@@ -9,7 +9,7 @@ export default function Contacts() {
     <div className="container-fluid pl-5 pr-5 pt-5 pb-3 p_top">
     
         <div className="row">
-        <div className="ml-auto mr-1 m_bottom">    
+        <div className="ml-auto mr-1 p_bottom">    
             <a href="https://www.linkedin.com/in/sandila-vihanga-79033b223/" target="_blank" rel="noreferrer" className="social_media_icons"> <LinkedInIcon/> </a>
             <a href="https://github.com/sandilavi" target="_blank" rel="noreferrer" className="social_media_icons"> <GitHubIcon/> </a>
         </div>   
@@ -20,15 +20,15 @@ export default function Contacts() {
         <form action="https://formspree.io/f/xnqkgoao" method="POST">
         <div className="row">
             <div className="col-12 col-md-6">
-                <input type="text" name='Name' placeholder='Name' className="form_fields center" required/>
+                <input type="text" name='Name' placeholder='Name' className="form_fields input_fields center" required/>
             </div>
 
             <div className="col-12 col-md-6">
-                <input type="email" name='Email' placeholder='Email' className="form_fields center" required/>
+                <input type="email" name='Email' placeholder='Email' className="form_fields input_fields center" required/>
             </div>
 
             <div className="col-12">
-                <input type="text" name='Subject' placeholder='Subject' className="form_fields center" required/>    
+                <input type="text" name='Subject' placeholder='Subject' className="form_fields input_fields center" required/>    
             </div>
 
             <div className="col-12">
