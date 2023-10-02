435417520cb025b123c4e653a55341ef7f41ac05
sandilavi <sandilavihanga@gmail.com>
2023-10-02 21:04:18 +0530

    Removed unwanted files from public/images folder

--- src/components/my_projects/Projects.jsx
+++ src/components/my_projects/Projects.jsx
@@ -15,11 +15,11 @@ export default function Projects() {
             <a href="https://whatsappcloneweb.netlify.app/"> <img src="images/whatsapp.png" className="img-fluid" alt=""/> </a>
         </div>
         </div>
-  
+    
         <div className="col-12 col-sm-6 col-lg-3">
         <div className="cards_projects">
             <p className="project_name"> Old Portfolio </p>
-            <a href="https://oldportfoliowebsite.netlify.app/"> <img src="images/download.png" className="img-fluid" alt=""/> </a>
+            <a href="https://oldportfolioweb.netlify.app/"> <img src="images/portfolio.png" className="img-fluid" alt=""/> </a>
         </div>
         </div>
           
